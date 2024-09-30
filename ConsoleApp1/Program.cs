d24eb6101a3b48e833e6562056c035041ecc6574
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-09-30 13:46:24 +0530

    ADDED changes to the usecase 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,10 @@ namespace ConsoleApp1
 {
     internal class EmployeeWageCompute
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the main page of Employee Wage");
+        }
        public void CheckAttendance()
         {
             Random r = new Random();
