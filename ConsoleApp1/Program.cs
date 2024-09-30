7f25187cd3c72d0f768eba08df007d155611ea10
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-09-30 16:56:31 +0530

    ADDED functionality to calculate using switch cases

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,38 +4,37 @@ namespace ConsoleApp1
 {
     internal class EmployeeWageCompute
     {
-
-
-        public void Caluculatewageswitch()
-        {
-            int isfulltime = 0;
-            int isparttime = 1;
-            int wageperhour = 20;
-            int fullhour = 8;
-            int parthour = 4;
-            int wage = 0;
-            string res;
-           
-            
-            Random r = new Random();
-            int checkemp = r.Next(0, 2);
-
-            switch (checkemp)
+            public void Caluculatewageswitch()
             {
-                case 0: wage = wageperhour * fullhour; break;
-                case 1: wage= wageperhour * parthour;break;
-            }
-            if (checkemp == 0)
-            {
-                res = "Full Time Employee";
-            }
-            else
-            {
-                res = "Part time Employee";
-            }
-            Console.WriteLine("Wage for " + res + " is " + wage);
+                int isfulltime = 0;
+                int isparttime = 1;
+                int wageperhour = 20;
+                int fullhour = 8;
+                int parthour = 4;
+                int wage = 0;
+                string res;
 
+
+                Random r = new Random();
+                int checkemp = r.Next(0, 2);
+
+                switch (checkemp)
+                {
+                    case 0: wage = wageperhour * fullhour; break;
+                    case 1: wage = wageperhour * parthour; break;
+                }
+                if (checkemp == 0)
+                {
+                    res = "Full Time Employee";
+                }
+                else
+                {
+                    res = "Part time Employee";
+                }
+                Console.WriteLine("Wage for " + res + " is " + wage);
+
+            }
         }
     }
-}
+
 
