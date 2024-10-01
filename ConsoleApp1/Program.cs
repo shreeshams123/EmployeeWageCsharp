f94e336e3401daa53512891f84204b642927834c
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-10-01 10:36:53 +0530

    Merged usecase 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,15 +4,43 @@ namespace ConsoleApp1
 {
     internal class EmployeeWageCompute
     {
-
-
-
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the main page of Employee Wage");
             EmployeeWageCompute e = new EmployeeWageCompute();
             e.CheckAttendance();
         }
+        public void Caluculatewageswitch()
+        {
+            int isfulltime = 0;
+            int isparttime = 1;
+            int wageperhour = 20;
+            int fullhour = 8;
+            int parthour = 4;
+            int wage = 0;
+            string res;
+
+            Random r = new Random();
+            int checkemp = r.Next(0, 2);
+
+            switch (checkemp)
+            {
+                case 0: wage = wageperhour * fullhour; break;
+                case 1: wage = wageperhour * parthour; break;
+            }
+            if (checkemp == 0)
+            {
+                res = "Full Time Employee";
+            }
+            else
+            {
+                res = "Part time Employee";
+            }
+            Console.WriteLine("Wage for " + res + " is " + wage);
+        }
+
+
+
 
         public void ComputePartTimeWage()
         {
@@ -20,6 +48,12 @@ namespace ConsoleApp1
             int wageperhour = 20;
             Console.WriteLine("Part time wage is" + parttimehour * wageperhour);
 
+        }
+        public void ComputeDailyWage()
+        {
+            int wageperhour = 20;
+            int dailyhour = 8;
+            Console.WriteLine("Daily wage is" + wageperhour * dailyhour);
         }
         public void CheckAttendance()
         {
@@ -32,17 +66,12 @@ namespace ConsoleApp1
             else if (isPresent == 1)
             {
                 Console.WriteLine("Employee is present");
+
+
+
             }
         }
-
-        public void ComputeDailyWage()
-        {
-            int wageperhour = 20;
-            int dailyhour = 8;
-            Console.WriteLine("Daily wage is" + wageperhour * dailyhour);
-
-        }
-
     }
 }
+
 
